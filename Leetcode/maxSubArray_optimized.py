80073f429f6ee90f11a0bf1416e763981e6b5142
rahulb5 <rhlbhagtani81@gmail.com>
2021-05-10 09:14:29 +0530

    Add files via upload

---
+++ Leetcode/maxSubArray_optimized.py
@@ -0,0 +1,15 @@
+def maxSubArray(nums):
+    
+    max_sum = nums[0]
+    cur_sum = 0
+    for i in nums:
+        cur_sum = cur_sum + i
+        if cur_sum < i:
+            cur_sum = i
+        if cur_sum > max_sum:
+            max_sum = cur_sum
+#        print(max_sum)
+    return max_sum
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(maxSubArray(nums))
